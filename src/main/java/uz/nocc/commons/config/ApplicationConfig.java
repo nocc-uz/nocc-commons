ce8ae170202c3ac5acb401a3fef82b12f512f49d
developer <murodullayev.saydali0080@gmail.com>
2023-12-13 16:59:08 +0500

    init

--- src/main/java/uz/nocc/commons/config/ApplicationConfig.java
+++ src/main/java/uz/nocc/commons/config/ApplicationConfig.java
@@ -6,15 +6,12 @@ import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.context.request.WebRequest;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * @author Jonibek Amirkulov (thejwa)
@@ -28,11 +25,6 @@ public class ApplicationConfig {
     @Bean("runner")
     public CommandLineRunner runner() {
         return args -> System.out.printf("START: %s%n", format.format(new Date()));
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
     }
 
     @Bean
